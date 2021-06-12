c2779c416d3f3e19304baf8258248ab297057a83
Jose A. Molina <josemouribe@gmail.com>
2021-06-12 17:30:15 +0200

    feat: Routing on main app file

--- client/tropical/App.js
+++ client/tropical/App.js
@@ -1,28 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar  } from 'react-native';
-import { NativeRouter, Route, Switch } from 'react-router-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { NativeRouter, Route, Switch, useParams } from 'react-router-native';
+import { useState, useEffect } from 'react';
 
 import Dashboard from './containers/dashboard/dashboard';
 import BeachInfo from './components/beachInfo/beachInfo';
 import NavBar from './components/navBar/navBar';
+import  getData  from './sevices/apiServices';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    getAll();
+  }, [])
+
+  const getAll = () => {
+    const data = getData();
+    setInfo(data);
+  }
+
+    return (
+      <View style={styles.container}>
         <NavBar />
         <Text> Hello from the main app</Text>
-
-    <NativeRouter  >
-        <Switch>
-          <Route exact path='/' component={Dashboard} />
-          <Route exact path='/beach' component={BeachInfo} />
-        </Switch>
-
-    </NativeRouter>
-
+        <NativeRouter >
+          <Switch>
+            <Route exact path='/'>
+            <Dashboard data={info} />
+            </Route>
+            <Route exact path='/beach'>
+              <BeachInfo data={info} />
+            </Route>
+          </Switch>
+        </NativeRouter>
         <StatusBar style="auto" />
       </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
