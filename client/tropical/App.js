aa6d74cb04663794b081ffc141482bd3ac287a23
Jose A. Molina <josemouribe@gmail.com>
2021-06-12 15:11:18 +0200

    feat: Routing

--- client/tropical/App.js
+++ client/tropical/App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Switch } from 'react-router-native';
+import Dashboard from './containers/dashboard/dashboard';
+import BeachInfo from './components/beachInfo/beachInfo';
+import NavBar from './components/navBar/navBar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> HI</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeRouter>
+
+      <View style={styles.container}>
+        <NavBar />
+        <Text> HI</Text>
+        <Switch>
+          <Route exact path='/'> <Dashboard /></Route>
+          <Route exact path='/beach'> <BeachInfo /></Route>
+        </Switch>
+        <StatusBar style="auto" />
+      </View>
+    </NativeRouter>
   );
 }
 
