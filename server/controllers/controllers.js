34850feec791b99ff1ce168b08bc4839edde6a48
Jose A. Molina <josemouribe@gmail.com>
2021-06-12 09:20:14 +0200

    Controlers

---
+++ server/controllers/controllers.js
@@ -0,0 +1,29 @@
+// const axios = require('axios');
+// const parser = require('xml-js');
+const BeachState = require('../models/scheme');
+
+const getData = async (req, res) => {
+  req.body = getInfo;
+  res.send('Sending it');
+}
+
+module.exports = getData;
+
+// PENDING
+
+/*
+const apiUrl = 'https://www.barcelona.cat/sites/all/static/platges/clabsa/estatactualplatges.xml';
+
+const getInfo = axios.get(apiUrl)
+.then(response => {
+  // response.text();
+  return response.data;
+})
+.then(str => {
+  const jsonFile = JSON.parse(parser.xml2json(str));
+  // func(jsonFile);
+})
+.catch(e => {
+  console.log('error: ', e);
+});
+*/
