04f8bfb5b3d8e6a7876520908c9bd3f241a17c44
Jose A. Molina <josemouribe@gmail.com>
2021-06-12 11:23:10 +0200

    feat: get route

--- server/router.js
+++ server/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { Router } = express;
 const router = Router();
+const  getData = require('./controllers/controllers');
 
-// router.get('/', something)
+router.get('/', getData)
 
 module.exports = router;
