d91340cc056a6d3dfbeb0eb560326c8f12956783
Jose A. Molina <josemouribe@gmail.com>
2021-06-12 16:45:27 +0200

    WIP : Routes and styling

--- client/tropical/App.js
+++ client/tropical/App.js
@@ -1,33 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar  } from 'react-native';
 import { NativeRouter, Route, Switch } from 'react-router-native';
+
 import Dashboard from './containers/dashboard/dashboard';
 import BeachInfo from './components/beachInfo/beachInfo';
 import NavBar from './components/navBar/navBar';
 
 export default function App() {
   return (
-    <NativeRouter>
-
-      <View style={styles.container}>
+    <View style={styles.container}>
         <NavBar />
-        <Text> HI</Text>
+        <Text> Hello from the main app</Text>
+
+    <NativeRouter  >
         <Switch>
-          <Route exact path='/'> <Dashboard /></Route>
-          <Route exact path='/beach'> <BeachInfo /></Route>
+          <Route exact path='/' component={Dashboard} />
+          <Route exact path='/beach' component={BeachInfo} />
         </Switch>
+
+    </NativeRouter>
+
         <StatusBar style="auto" />
       </View>
-    </NativeRouter>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'green',
+
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android'? StatusBar.currentHeight : 0
   },
 });
