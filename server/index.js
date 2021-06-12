d3991c10cc0f4ba94402f19e8a6f071c2315ae6a
Jose A. Molina <josemouribe@gmail.com>
2021-06-12 11:22:26 +0200

    feat:connecting DB

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const cors= require('cors');
 const app = express();
 
 app.use(cors());
+require('./models/db');
 
 const port = 3000;
 const router = require('./router');
 
+app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
