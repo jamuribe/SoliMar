e0ef1f45f5210706f80b1fd7cbc1fd85459104eb
Jose A. Molina <josemouribe@gmail.com>
2021-06-14 12:16:58 +0200

    feat: Prop drilling

--- client/tropical/App.js
+++ client/tropical/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
-import { NativeRouter, Route, Switch, useParams } from 'react-router-native';
-import { useState, useEffect } from 'react';
 import { enableScreens } from 'react-native-screens';
 
 import Dashboard from './containers/dashboard/dashboard';
 import BeachInfo from './components/beachInfo/beachInfo';
 import NavBar from './components/navBar/navBar';
-import  getData  from './sevices/apiServices';
 
 enableScreens();
 
@@ -17,15 +14,6 @@ import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [info, setInfo] = useState([]);
-  useEffect(() => {
-    getAll();
-  }, [])
-
-  const getAll = () => {
-    const data = getData();
-    setInfo(data);
-  }
 
   return (
     <NavigationContainer>
@@ -43,21 +31,6 @@ export default function App() {
 
       </Stack.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <NavBar />
-    //   <Text> Hello from the main app</Text>
-    //   <NativeRouter >
-    //     <Switch>
-    //       <Route exact path='/'>
-    //         <Dashboard data={info} />
-    //       </Route>
-    //       <Route exact path='/beach'>
-    //         <BeachInfo data={info} />
-    //       </Route>
-    //     </Switch>
-    //   </NativeRouter>
-    //   <StatusBar style="auto" />
-    // </View>
   );
 }
 
