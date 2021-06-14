840716fa7fccf3f8e0a9fc85f0896f917c8ea33a
Jose A. Molina <josemouribe@gmail.com>
2021-06-14 11:15:32 +0200

    feat: Mock data to render info

--- client/tropical/components/beachInfo/beachInfo.js
+++ client/tropical/components/beachInfo/beachInfo.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useHistory } from 'react-router-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
-export default function beachInfo() {
+export default function beachInfo({navigation}) {
 
-  const history = useHistory();
-
-  const handleClick = () => {
-    history.push('/');
-  };
+  const mockInfo = {
+      "name": "Sant Sebastià",
+      "waterQuality": "1",
+      "jellyfish": "1",
+      "seaState": "1",
+      "occupancy": "2",
+      "url": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftravelblogstories.com%2Fwp-content%2Fuploads%2F2018%2F12%2Fsan_sebastian_beach.jpg&f=1&nofb=1"
+    };
 
   return (
-    <View>
-      <Button title='Go Home' onPress={handleClick} />
-      <Text>I am the beach info</Text>
+    <View style={styles.beachContainer}>
+      <Button title='Home' onPress={() => navigation.navigate('Home')} />
+      <Text style={{color: '#888', fontSize: 18}}>
+        {mockInfo.name}
+      </Text>
+      <Image source={{uri:mockInfo.url}} style={{ width: 305, height: 159 }} />
     </View>
   )
 }
@@ -21,8 +26,7 @@ export default function beachInfo() {
 const styles = StyleSheet.create({
    beachContainer: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 })
