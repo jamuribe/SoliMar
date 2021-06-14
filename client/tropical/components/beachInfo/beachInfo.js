cee37e10f39950528e5f4230aca51b0bc8ac627f
Jose A. Molina <josemouribe@gmail.com>
2021-06-14 22:12:57 +0200

    feat: Rendering params

--- client/tropical/components/beachInfo/beachInfo.js
+++ client/tropical/components/beachInfo/beachInfo.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import  ProgressBar  from '../../animation/progressBar';
 
-export default function beachInfo({navigation, data, route}) {
-
-  const mockInfo = {
-    "name": "Sant Sebastià",
-    "waterQuality": "1",
-    "jellyfish": "1",
-    "seaState": "1",
-    "occupancy": "2",
-    "url": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftravelblogstories.com%2Fwp-content%2Fuploads%2F2018%2F12%2Fsan_sebastian_beach.jpg&f=1&nofb=1"
-  };
-
+export default function beachInfo({navigation, route}) {
   const params = route.params;
-
+  console.log(params.data);
   return (
-    <View style={styles.beachContainer}>
+    <View /*style={styles.beachContainer}*/>
       <Button title='Home' onPress={() => navigation.navigate('Home')} />
       <Text style={{color: '#888', fontSize: 18}}>
-        {mockInfo.name}
+        {params.data.name}
       </Text>
-      <Image source={{uri:mockInfo.url}} style={{ width: 305, height: 159 }} />
-      <Text>Occupancy: {mockInfo.occupancy}</Text>
+      <Image source={{uri:params.data.url}} style={{ width: 305, height: 159 }} />
+
+      <Text>Occupancy:  </Text>
+      <ProgressBar step={params.data.occupancy}/>
+      <Text>Jellyfish: </Text>
+      <ProgressBar step={params.data.jellyfish}/>
+      <Text>Sea State: </Text>
+      <ProgressBar step={params.data.seaState}/>
+
     </View>
   )
 }
@@ -29,7 +27,7 @@ export default function beachInfo({navigation, data, route}) {
 const styles = StyleSheet.create({
    beachContainer: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
+    backgroundColor: '#FAF0CA',
+    // alignItems: 'center',
   },
 })
