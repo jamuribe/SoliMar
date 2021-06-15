c953f771742d8f38af34174a0e5b70c3bb5f974e
Jose A. Molina <josemouribe@gmail.com>
2021-06-15 19:42:22 +0200

    fix: Styling

--- client/tropical/components/beachInfo/beachInfo.js
+++ client/tropical/components/beachInfo/beachInfo.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, useWindowDimensions } from 'react-native';
 import  ProgressBar  from '../../animation/progressBar';
 
 export default function beachInfo({navigation, route}) {
+  const { width, height } = useWindowDimensions();
   const params = route.params;
-  console.log(params.data);
   return (
-    <View /*style={styles.beachContainer}*/>
+    <>
+    <View style={styles.beachContainer}>
       <Button title='Home' onPress={() => navigation.navigate('Home')} />
       <Text style={{color: '#888', fontSize: 18}}>
         {params.data.name}
       </Text>
-      <Image source={{uri:params.data.url}} style={{ width: 305, height: 159 }} />
+      <Image source={{uri:params.data.url}} style={{ width:'90%', height: '40%' }} />
 
-      <Text>Occupancy:  </Text>
-      <ProgressBar step={params.data.occupancy}/>
-      <Text>Jellyfish: </Text>
-      <ProgressBar step={params.data.jellyfish}/>
-      <Text>Sea State: </Text>
-      <ProgressBar step={params.data.seaState}/>
-
+      <View style={{backgroundColor:'#F4D35E', width, margin:'5%', display:'flex', height:'40%'}}>
+        <Text>Occupancy:  </Text>
+        <ProgressBar step={params.data.occupancy}/>
+        <Text>Jellyfish: </Text>
+        <ProgressBar step={params.data.jellyfish}/>
+        <Text>Sea State: </Text>
+        <ProgressBar step={params.data.seaState}/>
+      </View>
     </View>
+
+    </>
   )
 }
 
@@ -28,6 +32,6 @@ const styles = StyleSheet.create({
    beachContainer: {
     flex: 1,
     backgroundColor: '#FAF0CA',
-    // alignItems: 'center',
+    alignItems: 'center',
   },
 })
