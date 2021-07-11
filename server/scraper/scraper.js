51f60cca095baf1dde1330a9a64a0978f7238064
Jose A. Molina <josemouribe@gmail.com>
2021-07-11 18:00:54 +0200

    Xml parser and data scraper

---
+++ server/scraper/scraper.js
@@ -0,0 +1,44 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+const apiUrl = 'https://www.barcelona.cat/sites/all/static/platges/clabsa/estatactualplatges.xml';
+const state = {};
+
+request(apiUrl, (error, response, data) => {
+  if (!error && response.statusCode === 200) {
+
+    const $ = cheerio.load(data, {
+      xml: {
+        withDomLvl1: true,
+        normalizeWhitespace: false,
+        xmlMode: true,
+        decodeEntities: true
+      }
+    });
+
+    state.lastActualization = $('dataActualitzacio').text();
+
+    $('infoMET').each((index, el) => {
+      state.waterTemp = $(el).children('temperaturaAigua').text()
+      state.temp = $(el).children('temperaturaAmbient').text()
+      state.UVIndex = $(el).children('radiacioUltraviolada').text()
+      state.morning = $(el).children('previsioTempsMatiAvui').text()
+      state.afternoon = $(el).children('previsioTempsTardaAvui').text()
+      state.maxTemp = $(el).children('temperaturaMaxima').text()
+      state.minTemp = $(el).children('temperaturaMinima').text()
+    })
+
+    $('Platja').each((index, el) => {
+      state[beachName = $(el).children('nomPlatja').text()] = {
+        flagColor: $(el).children('estatBandera').text(),
+        waterQuality: $(el).children('qualitatAigua').text(),
+        jellyfish: $(el).children('Meduses').text(),
+        seaCondition: $(el).children('estatMar').text(),
+        swimmAllowed: $(el).children('permisBany').text(),
+        crowd: $(el).children('ocupacio').text()
+      }
+    });
+  }
+})
+
+module.exports = state;
